a026c89a0a733c80fbe92ffb7fe2492c4db23c6e
Kody-J <36755234+Kody-J@users.noreply.github.com>
2019-03-11 02:57:38 -0500

    Update Dealer.java

--- src/Dealer.java
+++ src/Dealer.java
@@ -95,4 +95,45 @@ public class Dealer {
             dealerHand[i].print();
         }
     }
+    //This is the begenning of the 5 card Poker logic: pretty simple implemintaion as such
+    //1. Fiver cards are delt to each player
+    //2. Betting round #1 =, begenning with the first active player to left of bing blind 
+    //3. Draw (This where each player is given the opportunity to 'discard' any or all thier cards and recive new replacments
+    //4. Betting round #2, begenning with the first active player
+    //5. Showdown (a winner is determined if thier is more than one player in the pot)
+     public void poker_first(Player p) {
+        int[] index_to_remove = new int[-5];
+        //create a text pop up window to display intermediate instructions to the screen
+        //accept use input to identify card form user hand to discard and put them in index_to_remove
+        for (int i = 0; i < 5; i++) {
+            if (p.getHand ( ) == null || index_to_remove[i] < 0 || index_to_remove[i] >= p.getHand ( ).length) {
+                // Create another array of size one less
+                Card[] anotherArray = new Card[p.getHand ( ).length - 1];
+                // Copy the elements except the index from original array to the other array
+                for (int k = 0; k < p.getHand ( ).length; k++) {
+                    // if the index is the removal element index
+                    if (i == index_to_remove[i]) {
+                        continue;
+                    }
+
+                    // if the index is not the removal element index
+                    anotherArray[k++] = p.getHand ( )[i];
+                }
+
+            }
+        }
+    }
+
+    public void take_bets() {
+        for(int h = 0; h < players.length; h++)
+            players[h].raise_check_fold_match ();
+
+    }
+
+    public void five_card_poker(){
+        for (int j = 0; j < players.length; j++) {
+            poker_first (players[j]);
+        }
+    }
+}
 }
