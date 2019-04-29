3a3d7382fec75113eed64c60d5bb376a287aa054
Kody-J <36755234+Kody-J@users.noreply.github.com>
2019-04-29 02:53:33 -0500

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,67 +1,40 @@
-
 package com.lab1.hothands;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Player {
 
-    private int count;
     private String name;
-    private Card[] hand ;
-    private int money;
-    private boolean is_in, check, fold;
+    private ArrayList<Card> hand;
+    private int chips;
+    private boolean bet, check, fold, call;
 
-    public Player(){
-        name = "NO NAME";
-        hand = new Card [5];
-        money = -1;
+    public Player() {
+        name = "A/I";
+        hand = new ArrayList<Card> ();
+        chips = 50000; //each new player gets 50k in chips
     }
+
     //This  is for texas holdem: Player receives two cards to start
-    public Player(String _name, Card one, Card two){
-        name = _name;
-        hand[0] = one;
-        hand[1] = two;
-        count = 2;
+    public Player(String nameData){
+        name = nameData;
+        chips = 50000;
+        hand = new ArrayList<Card>();
     }
-    // This is for 5 card poker: Player gets five cards to start
-    public Player(String _name, Card one, Card two, Card three, Card four, Card five){
-        name = _name;
-        hand[0] = one;
-        hand[1] = two;
-        hand[2] = three;
-        hand[3] = four;
-        hand[4] = five;
-        count = 5;
-    };
-
-    // returns the players cards
-    public Card[] getHand(){return hand;}
-
-
-    // Allows the player to see card
-    //TO DO: Make it display as a card image on the screen
-    public void peek_hand(){
-        for(int i = 0 ; i < hand.length; i++)
-        {
-            hand[i].print();
-            System.out.println();
-        }
+    public ArrayList<Card> getHand() {
+        return hand;
     }
-    //Adds a card to the players hand
-    public void setHand(Card c){hand[count++] = c;}
+    public int getChips(){return chips;}
+    public void setCard(Card c) {
+        hand.add(c);
+    }
     //Returns payers name
-    public String getName(){return name;}
-    //Sets players name
-    public void setName(){
-        System.out.println("Please Enter Your Name");
-        Scanner keyboard = new Scanner(System.in);
-        name = keyboard.nextLine();
-        while(name == " "){
-            name = keyboard.nextLine();
-        }
+    public String getName() {
+        return name;
     }
-    //Displays a single card
-    //TO DO: implement in one function with printHand() also display a card image
-    public void print(int i){hand[i].print();}
 
+    public int getCardId(int i){
+        return hand.get (i).getId ();
+    }
 }
